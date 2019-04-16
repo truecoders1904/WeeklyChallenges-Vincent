22ff8368c96a3d8d6ee33cd82ccb5a7873eebc12
Vincent <vincenttafolla1@gmail.com>
2019-04-16 14:15:00 -0500

    fixed the first question

--- ChallengesWithTestsMarkVII/ChallengesSet01.cs
+++ ChallengesWithTestsMarkVII/ChallengesSet01.cs
@@ -8,7 +8,11 @@ namespace ChallengesWithTestsMarkVII
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
-            throw new NotImplementedException();
+            if (num1 == num2)
+                return true;
+            else
+                return false;
+
         }
 
         public double Subtract(double minuend, double subtrahend)
