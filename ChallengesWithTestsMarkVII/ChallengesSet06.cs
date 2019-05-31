67f41630292f3474cb816f2802ddcddf8ef52403
Vincent <vincenttafolla1@gmail.com>
2019-05-31 16:36:04 -0500

    finished week 6

--- ChallengesWithTestsMarkVII/ChallengesSet06.cs
+++ ChallengesWithTestsMarkVII/ChallengesSet06.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMarkVII
 {
@@ -7,27 +8,99 @@ namespace ChallengesWithTestsMarkVII
     {
         public bool CollectionContainsWord(IEnumerable<string> words, string word, bool ignoreCase)
         {
-            throw new NotImplementedException();
+            if (words == null || words.Contains(null))
+            {
+                return false;
+            }
+            return ignoreCase ?
+                words.Any(s => s.Equals(word, StringComparison.OrdinalIgnoreCase)) :
+                words.Contains(word);
         }
 
         public bool IsPrimeNumber(int num)
         {
-            throw new NotImplementedException();
+            if (num <= 1)
+            {
+                return false;
+            }
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                    return false;
+            }
+            return true;
         }
 
         public int IndexOfLastUniqueLetter(string str)
         {
-            throw new NotImplementedException();
+            int uniques = -1;
+            bool isDuplicate;
+            for (int i = 0; i < str.Length; i++)
+            {
+                isDuplicate = false;
+
+                for (int j = 0; j < str.Length; j++)
+                {   
+                    if ((str[i] == str[j]) && i != j)
+                    {
+                        isDuplicate = true;
+                    }
+                }
+
+                if (isDuplicate == false)
+                {
+                    uniques = i;
+                }
+            }
+            return uniques;
         }
 
         public int MaxConsecutiveCount(int[] numbers)
         {
-            throw new NotImplementedException();
+            int count = 0;
+            int newCount = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                count = 0;
+                for (int j = i; j < numbers.Length; j++)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        count++;
+                    }
+                    else if (numbers[i] != numbers[j])
+                    {
+                        i = j - 1;
+                        break;
+                    }
+                }
+                if (count > newCount)
+                {
+                    newCount = count;
+                }                
+            }
+            return newCount;
+
         }
 
         public double[] GetEveryNthElement(List<double> elements, int n)
         {
-            throw new NotImplementedException();
+            List<double> result = new List<double>();
+
+            double[] arr = { };
+
+            if (elements == null || n < 0)
+            {
+                return arr;
+            }
+            for (int i = 0; i < elements.Count; i ++)
+            {
+                if (elements[i] % n == 0)
+                {
+                    result.Add(elements[i]);
+                }
+            }
+            return result.ToArray();
         }
     }
 }
